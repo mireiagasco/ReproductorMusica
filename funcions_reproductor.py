46797f7dad9ebdc27473155868bb66a8e0c3071e
Mireia Gasco Agorreta <63343593+mireiagasco@users.noreply.github.com>
2020-08-13 09:50:53 +0200

    Update funcions_reproductor.py

--- funcions_reproductor.py
+++ funcions_reproductor.py
@@ -7,7 +7,10 @@ import threading
 import time
 
 #Funció que crida les funcions que toqui segons el botó pitjat
-def switch(accio = None, listbox = None):
+def switch(**kwargs):
+
+    #inicialitzem el reproductor
+    mixer.init()
 
     #simulem les variables estàtiques que necessitarem en les diferents funcions
     switch.pausat = getattr(switch, "pausat", False)
@@ -18,23 +21,26 @@ def switch(accio = None, listbox = None):
     switch.index = getattr(switch, "index", 0)
     switch.temps = getattr(switch, "temps", 0)
 
+    #obtenim el valor de l'opció del diccionari d'arguments
+    opcio = kwargs.get("accio", "Error")
+
     #cridem la funció que toqui segons el botó pitjat
-    if accio == 0:
-        importar_musica(listbox = listbox)
-    elif accio == 1:
+    if opcio == 0:
+        importar_musica(kwargs.get("listbox", None))
+    elif opcio == 1:
+        aturar_programa(kwargs)
+    elif opcio == 2:
         pass
-    elif accio == 2:
-        pass
-    elif accio == 3:
+    elif opcio == 3:
         pass
 
 #Lambda que mostra el missatge amb la informació
 sobre_nosaltres = lambda: tkinter.messagebox.showinfo("Informació", "Aquest reproductor ha estat creat amb Python tkinter")
 
 #funció que carrega una cançó a la llista de reproducció
-def importar_musica(listbox = None):
+def importar_musica(llista):
     switch.fitxer = tkinter.filedialog.askopenfilename()
-    afegir_a_llista(switch.fitxer, listbox)
+    afegir_a_llista(switch.fitxer, llista)
 
 #funció que afegeix una cançó a una llista
 def afegir_a_llista(canço, llista):
@@ -42,3 +48,26 @@ def afegir_a_llista(canço, llista):
     llista.insert(switch.index, nom)
     switch.playlist.append(canço)
     switch.index += 1
+
+def eliminar_detalls(d_etiq):
+    #obtenim les etiquetes
+    etiqueta_nom = d_etiq.get("nom", "Error")
+    etiqueta_durada = d_etiq.get("durada", "Error")
+    etiqueta_durada_actual = d_etiq.get("durada_actual", "Error")
+    #eliminem el seu contingut
+    etiqueta_nom['text'] = " "
+    etiqueta_durada['text'] = " "
+    etiqueta_durada_actual['text'] = " "
+
+#funció que para la música
+def stop(etiquetes):
+   mixer.music.stop()
+   eliminar_detalls(etiquetes)
+
+#funció que tanca el programa
+def aturar_programa(dic_etiq):
+    #aturem la música
+    stop(dic_etiq)
+    #obtenim la finestra general i la tanquem
+    finestra_general = dic_etiq.get("finestra", "Error")
+    finestra_general.destroy()
